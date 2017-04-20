f23f462cf701bdb525bdc4dd8cffb4af60c45287
Gareth Stenson <Gareth Stenson>
2017-04-20 21:35:04 +0100

    initial md changes

---
+++ README.r
@@ -0,0 +1,15 @@
+**Week 4 Project for Getting and Cleaning Data**
+=================================================
+
+**Overview**
+-------------
+This project is demonstrate ones ability to collect, work with, and clean a data set. The goal is to prepare tidy data that can be used for later analysis. 
+
+
+**What is in this folder**
+---------------------------
+
+1) a tidy data set as described below, 
+2) a link to a Github repository with your script for performing the analysis, and 
+3) a code book that describes the variables, the data, and any transformations or work that you performed to clean up the data [here](http://url.com/ "Title"). 
+
